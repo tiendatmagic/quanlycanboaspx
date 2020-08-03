8238c0685fdedb6dd709f4af593291c4ec833fb7
tiendatmagic <tiendatmagic8@gmail.com>
2020-08-03 20:33:26 +0700

    fixlayout:

--- Quanlycanbo/Scripts/magic.js
+++ Quanlycanbo/Scripts/magic.js
@@ -1,5 +1,6 @@
 ﻿$(document).ready(function () {
     $("#ContentPlaceHolder1_GridView1 tr:even").css("backgroundColor", "rgb(253,225,230)");
+    $("#ContentPlaceHolder1_GridView2 tr:even").css("backgroundColor", "rgb(253,225,230)");
     $("input").addClass("form-control");
 });
 alert("© 2020 Copyright by Tiendatmagic");
