d2738c8aee1eea360df6ff7c25fb300dcfc3e126
tiendatmagic <tiendatmagic8@gmail.com>
2020-08-03 23:03:27 +0700

    add

--- Quanlycanbo/Scripts/magic.js
+++ Quanlycanbo/Scripts/magic.js
@@ -1,6 +1,10 @@
 ﻿$(document).ready(function () {
     $("#ContentPlaceHolder1_GridView1 tr:even").css("backgroundColor", "rgb(253,225,230)");
     $("#ContentPlaceHolder1_GridView2 tr:even").css("backgroundColor", "rgb(253,225,230)");
+
+    $("#ContentPlaceHolder1_DetailsView1 tr:even").css("backgroundColor", "rgb(253,225,230)");
+    $("#ContentPlaceHolder1_DetailsView2 tr:even").css("backgroundColor", "rgb(253,225,230)");
+
     $("input").addClass("form-control");
     $("#ContentPlaceHolder1_DetailsView1 > tbody > tr:nth-child(1) > td:nth-child(1)").text("Phòng ban ID");
     $("#ContentPlaceHolder1_DetailsView1 > tbody > tr:nth-child(2) > td:nth-child(1)").text("Tên phòng ban");
@@ -18,4 +22,4 @@
     $("#ContentPlaceHolder1_DetailsView2 > tbody > tr:nth-child(11) > td:nth-child(1)").text("Ghi chú");
 
 });
-//alert("© 2020 Copyright by Tiendatmagic");
+alert("© 2020 Copyright by Tiendatmagic");
